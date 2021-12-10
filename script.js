87ac332454a801e1600eed4f6fe135e4ed8b0f5d
ALI.ELKHECHEN@NRWBANK.DE <_2019EnterMyGit_>
2021-12-10 08:36:25 +0100

    dynamische Erzeugung der Zeichenflaeche

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
-let colorPicker = document.querySelector('input');
+let colorPicker = document.querySelector("input");
 
-let art_area = document.getElementById("art");
-art_area.addEventListener("contextmenu", e => e.preventDefault());
-
-// let pixel = '<div class="pixel" onclick="setPixelColor(this)" oncontextmenu="resetPixelColor(this)"></div>'
+let artArea = document.getElementById("art");
+artArea.addEventListener("contextmenu", (e) => e.preventDefault());
 
 function setPixelColor() {
   let penColor = document.getElementById("pen").value;
@@ -11,20 +9,20 @@ function setPixelColor() {
 }
 
 function resetPixelColor(pixel) {
-  this.style.backgroundColor = 'white';
+  this.style.backgroundColor = "white";
 }
 
 function createRow() {
   let row = document.createElement("div");
   row.className = "row";
-  
+
   return row;
 }
 
 function createPixel() {
   let pixel = document.createElement("div");
   pixel.className = "pixel";
-  pixel.style.backgroundColor = 'white';
+  pixel.style.backgroundColor = "white";
   pixel.addEventListener("click", setPixelColor);
   pixel.addEventListener("contextmenu", resetPixelColor);
 
@@ -34,38 +32,44 @@ function createPixel() {
 function checkColumnAndRowSize(column, row) {
   if (column === "" || row === "") {
     const user_info = document.getElementById("user-info");
-    user_info.style.backgroundColor = "red";
-    user_info.style.color = "rgb(87, 4, 4)";
+    user_info.style.backgroundColor = "#ffcccc";
+    user_info.style.color = "#e60000";
     user_info.innerHTML = "Spalten- und Zeilenangabe darf nicht leer sein.";
     return 1;
   }
-  
+
   return 0;
 }
 
+function clearArtArea() {
+  while (artArea.firstElementChild) {
+    artArea.removeChild(artArea.firstElementChild);
+  }
+}
 
 function createGrid() {
-  art_area.textContent = "";
+  // clear space before creating new grid
+  clearArtArea();
+
   const columnCount = document.getElementById("spalten").value;
   const rowCount = document.getElementById("zeilen").value;
-  
+
   const checkUserInput = checkColumnAndRowSize(columnCount, rowCount);
-  
+
   if (checkUserInput === 1) {
     return null;
   }
-  
-  for (let i = 0; i < rowCount; i++) {
-  // create row object
-  let row = createRow();
-  
-  for (let i = 0; i < columnCount; i++) {
-    const pixel = createPixel();
-    row.appendChild(pixel);
-  }
-  
-  art_area.appendChild(row);
-  
-  }
 
+  for (let i = 0; i < rowCount; i++) {
+    // create row object
+    let row = createRow();
+
+    for (let i = 0; i < columnCount; i++) {
+      const pixel = createPixel();
+      row.appendChild(pixel);
+    }
+
+    artArea.style.border = "solid 5px black";
+    artArea.appendChild(row);
+  }
 }
