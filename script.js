3c0174203a0668fcc8c5baaba501d0a9f02223c6
ALI.ELKHECHEN@NRWBANK.DE <_2019EnterMyGit_>
2021-12-13 10:51:01 +0100

    added default, preset colors to choose from

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+/*
+TODO: Weitere Ideen:
+0. weiter huebsch machen.
+1. Bild exportieren, z.B. mit third party bib: dom-to-image: https://github.com/tsayen/dom-to-image
+2. merke die letzten 5 ausgewaehlten Farben
+3. Malen nach Zahlen auf Basis existierenden Bildes
+4. Wenn man scrollt soll die Farbauswahl mit scrollen
+*/
+
+// convert rgb and rgba values to hex
+const rgba2hex = (rgba) =>
+  `#${rgba
+    .match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+\.{0,1}\d*))?\)$/)
+    .slice(1)
+    .map((n, i) =>
+      (i === 3 ? Math.round(parseFloat(n) * 255) : parseFloat(n))
+        .toString(16)
+        .padStart(2, "0")
+        .replace("NaN", "")
+    )
+    .join("")}`;
+
 let colorPicker = document.querySelector("input");
 
 let artArea = document.getElementById("art");
@@ -6,6 +28,12 @@ artArea.addEventListener("contextmenu", (e) => e.preventDefault());
 function setPixelColor() {
   let penColor = document.getElementById("pen").value;
   this.style.backgroundColor = penColor;
+}
+
+function setPresetPixelColor(presetColor) {
+  let penColor = document.getElementById("pen");
+  console.log(presetColor.style.backgroundColor);
+  penColor.value = rgba2hex(presetColor.style.backgroundColor);
 }
 
 function resetPixelColor(pixel) {
